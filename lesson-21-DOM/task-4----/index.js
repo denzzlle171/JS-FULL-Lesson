ddf44d842498734dfb4124ba50a5f236c62363e1
Denys <denzzlle171@gmail.com>
2021-09-02 20:50:55 +0300

    save 00002

--- lesson-21-DOM/task-4----/index.js
+++ lesson-21-DOM/task-4----/index.js
@@ -1,13 +1,15 @@
-// 'use strict';
+'use strict';
 
-export const getTitle = () => {
+// export
+const getTitle = () => {
   const textTitle = document.querySelector('.title');
   console.log(textTitle.textContent);
   return textTitle.textContent;
 };
 getTitle();
 
-export const getDescription = () => {
+// export
+const getDescription = () => {
   const textDesc = document.querySelector('.about');
   console.log(textDesc.innerText);
   return textDesc.innerText;
@@ -15,14 +17,16 @@ export const getDescription = () => {
 
 getDescription();
 
-export const getPlants = () => {
+// export
+const getPlants = () => {
   const textPlans = document.querySelector('.plans');
   console.log(textPlans.innerHTML);
   return textPlans.innerHTML;
 };
 getPlants();
 
-export const getGoal = () => {
+// export
+const getGoal = () => {
   const textGoal = document.querySelector('.goal');
   console.log(textGoal.outerHTML);
   return textGoal.outerHTML;
---
+++ lesson-21-DOM/task-5/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export const setTitle = (text) => {
+  const textTitle = document.querySelector('.title');
+  // textTitle.textContent = text;
+
+  console.log(textTitle);
+};
+setTitle('text');
