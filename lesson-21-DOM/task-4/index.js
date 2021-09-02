9236313e34050c0e56e4e874e9e9c4d18cd1f127
Denys <denzzlle171@gmail.com>
2021-09-02 19:43:51 +0300

    save00001

--- lesson-21-DOM/task-4/index.js
+++ lesson-21-DOM/task-4/index.js
@@ -1,26 +1,30 @@
-'use strict';
+// 'use strict';
 
 export const getTitle = () => {
-  const textTitle = document.querySelector('.title').textContent;
-  console.log(textTitle);
-  return textTitle;
+  const textTitle = document.querySelector('.title');
+  console.log(textTitle.textContent);
+  return textTitle.textContent;
 };
 getTitle();
 
 export const getDescription = () => {
-  const textDesc = document.querySelector('.about').innerText;
-  console.log(textDesc);
+  const textDesc = document.querySelector('.about');
+  console.log(textDesc.innerText);
+  return textDesc.innerText;
 };
+
 getDescription();
 
 export const getPlants = () => {
-  const textPlans = document.querySelector('.plans').innerHTML;
-  console.log(textPlans);
+  const textPlans = document.querySelector('.plans');
+  console.log(textPlans.innerHTML);
+  return textPlans.innerHTML;
 };
 getPlants();
 
 export const getGoal = () => {
-  const textGoal = document.querySelector('.goal').outerHTML;
-  console.log(textGoal);
+  const textGoal = document.querySelector('.goal');
+  console.log(textGoal.outerHTML);
+  return textGoal.outerHTML;
 };
 getGoal();
