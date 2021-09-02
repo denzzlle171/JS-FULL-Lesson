0114acacae33982d2c14143879aead3e142f5216
Denys <denzzlle171@gmail.com>
2021-09-02 21:13:25 +0300

    save 000005

--- lesson-21-DOM/task-4----/index.js
+++ lesson-21-DOM/task-4----/index.js
@@ -1,32 +1,27 @@
 'use strict';
 
-// export
-const getTitle = () => {
+export const getTitle = () => {
   const textTitle = document.querySelector('.title');
   console.log(textTitle.textContent);
   return textTitle.textContent;
 };
 // getTitle();
 
-// export
-const getDescription = () => {
+export const getDescription = () => {
   const textDesc = document.querySelector('.about');
   console.log(textDesc.innerText);
   return textDesc.innerText;
 };
-
 // getDescription();
 
-// export
-const getPlants = () => {
+export const getPlants = () => {
   const textPlans = document.querySelector('.plans');
   console.log(textPlans.innerHTML);
   return textPlans.innerHTML;
 };
 // getPlants();
 
-// export
-const getGoal = () => {
+export const getGoal = () => {
   const textGoal = document.querySelector('.goal');
   console.log(textGoal.outerHTML);
   return textGoal.outerHTML;
