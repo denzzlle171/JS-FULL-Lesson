001e7b961dce9e4a3657768cfaaa0ded47398f2d
Denys <denzzlle171@gmail.com>
2021-12-18 15:02:59 +0300

    save 18.12

--- lesson-34-http/task-3/task-0/index.js
+++ lesson-34-http/task-3/task-0/index.js
@@ -1,32 +1,19 @@
 const baseUrl = 'https://61b61e94c95dd70017d40e50.mockapi.io/api/v1/user';
 
-const form = document.querySelector('.login-form');
+const formElt = document.querySelector('.login-form');
 const submitBtn = document.querySelector('.submit-button');
-
+const allInput = document.querySelectorAll('.form-input');
+//--------------------------------------------------------validation
 const onInputChange = () => {
-  const valid = form.reportValidity();
+  const valid = formElt.reportValidity();
   if (!valid) {
-    //
-    if (submitBtn.hasAttribute('disabled')) {
-      return;
-    }
-    submitBtn.setAttribute('disabled');
-
-    //
+    submitBtn.setAttribute('disabled', 'true');
   } else {
     submitBtn.removeAttribute('disabled');
   }
 };
-
-const onClick = (e) => {
-  e.preventDefault();
-
-  userData = {
-    email: form.email.value,
-    name: form.name.value,
-    password: form.password.value,
-  };
-
+//--------------------------------------------------------server_request
+const sentUserData = (userData) => {
   fetch(baseUrl, {
     method: 'POST',
     headers: {
@@ -34,17 +21,29 @@ const onClick = (e) => {
     },
     body: JSON.stringify(userData),
   });
+};
 
+const getUserData = () => {
   fetch(baseUrl)
     .then((response) => response.json())
     .then((users) => {
       alert(JSON.stringify(users));
     });
+};
+//---------------------------------------------------------main_function
+const onClick = (e) => {
+  e.preventDefault();
 
-  form.email.value = '';
-  form.name.value = '';
-  form.password.value = '';
+  const userData = [...new FormData(formElt)].reduce(
+    (acc, [field, value]) => ({ ...acc, [field]: value }),
+    {}
+  );
+
+  sentUserData(userData);
+  getUserData();
+
+  allInput.forEach((input) => (input.value = ''));
 };
 
-form.addEventListener('input', onInputChange);
+formElt.addEventListener('input', onInputChange);
 submitBtn.addEventListener('click', onClick);
